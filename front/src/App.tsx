61023a7b3e893303dc1e3e1df443d315a151dc1f
CanseFr <jucansell@gmail.com>
2025-08-25 12:43:23 +0200

    :sparkles: Create request on backend

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-// import {Typography} from "@mui/material";
 
 function App() {
-  return (
+
+    const[message, setMessage] = useState('');
+
+    async function fetchWithFallback() {
+        try {
+            const res = await fetch("http://deploy-service:3001/");
+            setMessage(res.statusText);
+        } catch (err) {
+            console.warn("Échec sur deploy-service, tentative sur localhost...", err);
+            try {
+                // 2. Fallback sur localhost
+                const res = await fetch("http://localhost:3001/");
+                setMessage(res.statusText);
+            } catch (err2) {
+                console.error("Échec sur localhost aussi :", err2);
+                setMessage("Aucune réponse du backend");
+            }
+        }
+    }
+
+
+    useEffect(() => {
+         fetchWithFallback()
+    })
+
+    if(!message) return <p>Loading ...</p>
+    // if(!message) return <CircularProgress variant="solid" />
+
+
+    return (
     <div className="App">
       <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        {/*  <Typography>Votre configuration</Typography>*/}
-        {/*  <Typography>Votre configuration</Typography>*/}
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-          Edit <code>src/App.tsx</code> and save to reload.
-          Edit <code>src/App.tsx</code> and save to reload.
-          Edit <code>src/App.tsx</code> and save to reload.
-          Edit <code>src/App.tsx</code> and save to reload.
+            {message}
         </p>
         <a
           className="App-link"
@@ -23,10 +44,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-          Learn React
         </a>
       </header>
-          {/*<Typography>Votre configuration</Typography>*/}
     </div>
   );
 }
