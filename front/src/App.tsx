4cb5f3d0d1635e4d79a469f8f01ce77a6e0dd00b
CanseFr <jucansell@gmail.com>
2025-08-25 13:07:22 +0200

    :rocket: Fix request

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -6,20 +6,14 @@ function App() {
     const[message, setMessage] = useState('');
 
     async function fetchWithFallback() {
-        try {
-            const res = await fetch("http://deploy-service:3001/");
-            setMessage(res.statusText);
-        } catch (err) {
             console.warn("Échec sur deploy-service, tentative sur localhost...", err);
             try {
-                // 2. Fallback sur localhost
                 const res = await fetch("http://localhost:3001/");
                 setMessage(res.statusText);
             } catch (err2) {
                 console.error("Échec sur localhost aussi :", err2);
                 setMessage("Aucune réponse du backend");
             }
-        }
     }
 
 
